20e3fbb104911fbf640505f9303d37a93d591ef8
cedric clemenceau <cedric.clemenceau@epitech.eu>
2017-10-23 02:52:40 +0200

    log file activate

--- thread.py
+++ thread.py
@@ -26,21 +26,20 @@ class decrypte(Thread):
     # La boucle de calcule
     def run(self):
         find = False
-        passe = False
+        i = 0
         
         for length in range(6, 13):
             if (find == False):
                 to_attempt = product(self.charList, repeat=length);
                 start = time.time();
                 for attempt in to_attempt:
-                    mdp = "".join(attempt);
+                    mdp = "".join(attempt)
                     now = time.time()
-                    if ((round(now) - round(start)) % 600 == 0 and passe == False):
+                    if (i % 10000000 == 0):
                         with RLock():
-                            recovery = open('recovery', 'a')
+                            recovery = open('log', 'a')
                             recovery.write('Thread is ' + self.nom + ' mdp is ' + mdp + ' time is : ' + str((now - start)) + '\n')
                             recovery.close();
-                            passe = True
                     if (self.shadowLine[1] == "1"):
                         h = hashlib.md5(mdp.encode("UTF-8").strip()).hexdigest();
                         if (h == self.shadowLine[2]):
@@ -53,9 +52,7 @@ class decrypte(Thread):
                         h = hashlib.sha512(mdp.encode("UTF-8").strip()).hexdigest();
                         if (h == self.shadowLine[2]):
                             find = self.findPass(mdp, start);
-                            print(self, mdp);
-                    if (round(now) - round(start) % 10 == 0):
-                        passe = False
+                    i += 1
                     if (find == True):
                         break;
 
